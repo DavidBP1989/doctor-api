1f565ed7d6a847b805432bab915187b7165343cf
David Bustamante <bustamante24.1989@gmail.com>
2021-03-17 21:13:36 -0600

    se modifico la vista vpatients y se agregaron referencias al modelo

--- doctor/doctor/Models/Patients/Patients.cs
+++ doctor/doctor/Models/Patients/Patients.cs
@@ -9,6 +9,7 @@ namespace doctor.Models.Patients
         public string LastName { get; set; }
         public int AgeInMonths { get; set; }
         public DateTime? LastConsultation { get; set; }
+        public DateTime? ExpirationDate { get; set; }
         public string EMECI { get; set; }
         public string Sex { get; set; }
         public DateTime? BirthDate { get; set; }
